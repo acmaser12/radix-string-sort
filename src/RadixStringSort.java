e65609a266ed4f579b21dc9ae8a98e621a1cf196
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-01-30 23:47:54 -0600

    Moved algorithms into methods per assignment spec.

--- src/RadixStringSort.java
+++ src/RadixStringSort.java
@@ -11,7 +11,6 @@ public class RadixStringSort {
         System.out.println("I certify that this is my own work");
 
         // create array of buckets
-        // TODO: Change this to Collection object later
         LinkedList<String>[] buckets = new LinkedList[27];
 
         // create list to store words
@@ -25,39 +24,64 @@ public class RadixStringSort {
             System.exit(2);
         }
 
+        // instantiate each bucket
         for (int i = 0; i < 27; i++) {
             buckets[i] = new LinkedList<>();
         }
 
-
-        // project spec states words are no longer than 10 letters, so we will loop 10 times
+        // project spec states words are no longer than 10 letters, so we will iterate 10 times
         for (int i = 10; i > 0; i--) {
-            for (String word : words) {
-                // pointer determines which bucket the word goes into
-                int pointer = 0;
-                if (word.length() >= i) {
-                    pointer = word.charAt(i-1) - 96;  // offset by 96 for ASCII char values
-                }
-                buckets[pointer].addLast(word);
-            }
-            // empty words list
+            // perform this iteration's radix sort
+            buckets = performRadixSortIteration(words, buckets, i);
+
+            // empty words list (to allow it to be reused)
             words.clear();
-            for (LinkedList<String> bucket : buckets) {
-                int size = bucket.size();
-                for (int j = 0; j < size; j++) {
-                    // add first element in list
-                    words.add(bucket.getFirst());
-                    // pop first element out of list
-                    bucket.removeFirst();
-                }
-            }
+
+            // extract words from buckets
+            words = extractFromBuckets(buckets, words);
+
+            // output words
             System.out.println("\nIterating on index: " + i);
             System.out.print("\t");
-            for (String word : words) {
-                System.out.print(word + " ");
-            }
-            System.out.println();
+            displayWords(words);
         }
+    }
+    private static LinkedList<String>[] performRadixSortIteration(LinkedList<String> words, LinkedList<String>[] buckets, int iteration) {
+        for (String word : words) {
+            // pointer determines which bucket the word goes into
+            int pointer = 0;
+            if (word.length() >= iteration) {
+                pointer = word.charAt(iteration - 1) - 96;  // offset by 96 for ASCII char values
+            }
+            buckets = placeInBucket(buckets, pointer, word);
+        }
+
+        return buckets;
+    }
+
+    private static LinkedList<String>[] placeInBucket(LinkedList<String>[] buckets, int bucketLocation, String word) {
+        buckets[bucketLocation].addLast(word);
+        return buckets;
+    }
+
+    private static LinkedList<String> extractFromBuckets(LinkedList<String>[] buckets, LinkedList<String> words) {
+        for (LinkedList<String> bucket : buckets) {
+            int size = bucket.size();
+            for (int j = 0; j < size; j++) {
+                // add first element in list
+                words.add(bucket.getFirst());
+                // pop first element out of list
+                bucket.removeFirst();
+            }
+        }
+        return words;
+    }
+
+    private static void displayWords(LinkedList<String> words) {
+        for (String word : words) {
+            System.out.print(word + " ");
+        }
+        System.out.println();
     }
 
     private static LinkedList<String> loadWords(File wordFile) throws FileNotFoundException {
